04d52412b43e7b47c2e20f050c9ba37e62467cd2
orsnken <hiroshima@net.itc.nagoya-u.ac.jp>
2019-01-30 17:41:34 +0900

    Add parameters and branch for MAC type

--- Run.cc
+++ Run.cc
@@ -15,33 +15,33 @@ using namespace WirelessLan;
 
 namespace {
 
-void output(FlowMonitorHelper& flowMonitor, Ptr<FlowMonitor> fm) {
-  fm->CheckForLostPackets ();
-  Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowMonitor.GetClassifier());
-  std::map<FlowId, FlowMonitor::FlowStats> stats = fm->GetFlowStats();
-  std::cout << "--------------------------------------" << std::endl;
-  std::vector<double> throughputs_kbps;
-  for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator iter = stats.begin(); iter != stats.end(); iter++) {
-    Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (iter->first);
-    double throughput_kbps = iter->second.rxBytes * 8.0 / (iter->second.timeLastRxPacket.GetSeconds() - iter->second.timeFirstTxPacket.GetSeconds()) / 1024;
-    NS_LOG_UNCOND("Flow ID: " << iter->first << " src addr " << t.sourceAddress << " dest addr " << t.destinationAddress);
-    NS_LOG_UNCOND("TP:" << throughput_kbps << ", Tx:" << iter->second.txBytes << ", Rx:" << iter->second.rxBytes << ", LP:" << iter->second.lostPackets);
-    throughputs_kbps.push_back(throughput_kbps);
-  }
-  
-  double fairness = 0.0;
-  double total_throughput_kbps = 0.0;
-  
-  for (double v: throughputs_kbps) {
-    fairness += v * v;
-    total_throughput_kbps += v;
-  }
-  fairness = total_throughput_kbps * total_throughput_kbps / (static_cast<double>(throughputs_kbps.size()) * fairness);
+double gSsmtAlpha = 0.5;
+double gSsmtPsi   = 4.0;
+double gSsmtZeta  = 0.974;
 
-  NS_LOG_UNCOND("SUM.: " << total_throughput_kbps);
-  NS_LOG_UNCOND("AVR.: " << total_throughput_kbps / static_cast<double>(throughputs_kbps.size()));
-  NS_LOG_UNCOND("FRN.: " << fairness);
+double gDistanceWlans     = 10.0;
+double gDistanceTerminals = 0.5;
 
+std::string gMacType = "SSMT_CI";
+
+void InitMacWithSsmt() {
+  Config::SetDefault("ns3::SsmtTxop::Alpha", DoubleValue(gSsmtAlpha));
+  Config::SetDefault("ns3::SsmtTxop::Psi"  , DoubleValue(gSsmtPsi));
+  Config::SetDefault("ns3::SsmtTxop::Zeta" , DoubleValue(gSsmtZeta));
+
+  Domain::Init();
+}
+
+void InitMacWithSrb() {
+  exit(1);
+}
+
+void InitMacWithCwOnly() {
+  exit(1);
+}
+
+void InitMacWithDcf() {
+  exit(1);
 }
 
 ApplicationContainer SetOnOffApplication(
@@ -83,42 +83,92 @@ ApplicationContainer SetApplication(
   return app;
 }
 
+void Output(FlowMonitorHelper& flowMonitor, Ptr<FlowMonitor> fm) {
+  fm->CheckForLostPackets ();
+  Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowMonitor.GetClassifier());
+  std::map<FlowId, FlowMonitor::FlowStats> stats = fm->GetFlowStats();
+  std::cout << "--------------------------------------" << std::endl;
+  std::vector<double> throughputs_kbps;
+  for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator iter = stats.begin(); iter != stats.end(); iter++) {
+    Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (iter->first);
+    double throughput_kbps = iter->second.rxBytes * 8.0 / (iter->second.timeLastRxPacket.GetSeconds() - iter->second.timeFirstTxPacket.GetSeconds()) / 1024;
+    NS_LOG_UNCOND("Flow ID: " << iter->first << " src addr " << t.sourceAddress << " dest addr " << t.destinationAddress);
+    NS_LOG_UNCOND("TP:" << throughput_kbps << ", Tx:" << iter->second.txBytes << ", Rx:" << iter->second.rxBytes << ", LP:" << iter->second.lostPackets);
+    throughputs_kbps.push_back(throughput_kbps);
+  }
+  
+  double fairness = 0.0;
+  double total_throughput_kbps = 0.0;
+  
+  for (double v: throughputs_kbps) {
+    fairness += v * v;
+    total_throughput_kbps += v;
+  }
+  fairness = total_throughput_kbps * total_throughput_kbps / (static_cast<double>(throughputs_kbps.size()) * fairness);
+
+  NS_LOG_UNCOND("SUM.: " << total_throughput_kbps);
+  NS_LOG_UNCOND("AVR.: " << total_throughput_kbps / static_cast<double>(throughputs_kbps.size()));
+  NS_LOG_UNCOND("FRN.: " << fairness);
+
+}
+
+
 } // namespace
 
 void Framework::Simulation::Init(int argc, char* argv[]) {
   CommandLine cmd;
-
+  cmd.AddValue("distanceWlans",
+              "The distance between each Wireless LANs.",
+              gDistanceWlans);
+  cmd.AddValue("distanceTerminals",
+               "The distance between AP and STAs.",
+               gDistanceTerminals);
+  cmd.AddValue("macType",
+               "The MAC algorithm applied with each terminals (SSMT_CI/SRB/CW_ONLY/DCF).",
+               gMacType);
+  cmd.AddValue("ssmtAlpha",
+               "The parameter associated with a successful transmission behavior for SSMT/CI.",
+               gSsmtAlpha);
+  cmd.AddValue("ssmtPsi",
+               "The parameter associated with a failed transmission behavior for SSMT/CI.",
+               gSsmtPsi);
+  cmd.AddValue("ssmtZeta",
+               "The upper limit of deterministic back-off probability for SSMT/CI.",
+               gSsmtZeta);
   cmd.Parse(argc, argv);
   
   Config::SetDefault("ns3::WifiRemoteStationManager::RtsCtsThreshold", UintegerValue(2200));
-
-  Config::SetDefault("ns3::SsmtTxop::Alpha", DoubleValue(0.0));
-  Config::SetDefault("ns3::SsmtTxop::Psi"  , DoubleValue(6.0));
-  Config::SetDefault("ns3::SsmtTxop::Zeta" , DoubleValue(0.9));
-
+  
   LogComponentEnable("SsmtTxop", LOG_INFO);
   LogComponentEnable("SsmtTxop", LOG_WARN);
 
-  // LogComponentEnable("Txop", LOG_LEVEL_FUNCTION);
-
-  Domain::Init();
+  if (gMacType == "SSMT_CI") {
+    InitMacWithSsmt();
+  } else if (gMacType == "SRB") {
+    InitMacWithSrb();
+  } else if (gMacType == "CW_ONLY") {
+    InitMacWithCwOnly();
+  } else if (gMacType == "DCF") {
+    InitMacWithDcf();
+  } else {
+    NS_LOG_UNCOND("The unknown MAC type \"" << gMacType << "\". Simulatinon aborted.");
+    exit(1);
+  }
 }
 
 void Framework::Simulation::Run() {
   std::cout << "Hello world!" << std::endl;
 
-  const double distance = 15.0;
-
   Domain network1("Network 1", "192.168.1.0", "255.255.255.0", 1);
-  network1.ConfigureMobility(Vector3D(0.0, 0.0, 0.0), 0.5);
+  network1.ConfigureMobility(Vector3D(0.0, 0.0, 0.0), gDistanceTerminals);
   SetApplication(network1, 1001, 0.1, 4.9);
 
   Domain network2("Network 2", "192.168.2.0", "255.255.255.0", 1);
-  network2.ConfigureMobility(Vector3D(distance, 0.0, 0.0), 0.5);
+  network2.ConfigureMobility(Vector3D(gDistanceWlans, 0.0, 0.0), gDistanceTerminals);
   SetApplication(network2, 2001, 0.1, 4.9);
 
   Domain network3("Network 3", "192.168.3.0", "255.255.255.0", 1);
-  network3.ConfigureMobility(Vector3D(distance / 2.0, distance / 2.0 * 1.7320508, 0.0), 0.5);
+  network3.ConfigureMobility(Vector3D(gDistanceWlans / 2.0, gDistanceWlans / 2.0 * 1.7320508, 0.0), gDistanceTerminals);
   SetApplication(network3, 3001, 0.1, 4.9);
 
   // AnimationInterface anim("SSMT-anim.xml");
@@ -131,5 +181,5 @@ void Framework::Simulation::Run() {
   Simulator::Run();
   Simulator::Destroy();
 
-  output(flowMonitor, fm);
+  Output(flowMonitor, fm);
 }
